d58b39dd842464c2c4c436d7cff0aace1c7e379c
Tu Nombre <you@example.com>
2025-04-20 11:00:21 -0600

    Update

--- .next/static/chunks/app_login_page_tsx_e04f1e08._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__dc44b95b._.js"
-],
-    source: "dynamic"
-});
