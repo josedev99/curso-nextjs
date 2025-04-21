87f61a8e9311fdd0ba29374d1bba2e086e6ee881
José Deodanes <60304928+josedev99@users.noreply.github.com>
2025-04-21 08:00:42 -0600

    Update middleware.ts
    ...

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,14 @@ export default NextAuth(authConfig).auth;
  
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  //matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: [
+     /*
+      * Middleware se aplica a todas las rutas excepto:
+      * - /api/*
+      * - /_next/*
+      * - archivos estáticos como .png, .jpg, .ico
+      */
+     '/((?!api|_next|favicon.ico|.*\\.(png|jpg|jpeg|svg|ico)$).*)',
+   ],
 };
