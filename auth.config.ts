c8d69e9318b98977b97c0ca3425a1bd40bcb0225
jose dev <josededanes99@gmail.com>
2025-04-28 23:57:11 -0600

    Recontruir auth

--- auth.config.ts
+++ auth.config.ts
@@ -5,17 +5,20 @@ export const authConfig = {
     signIn: '/login',
   },
   callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
+    authorized({ auth, request }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const isOnDashboard = request.nextUrl.pathname.startsWith('/dashboard');
+    
       if (isOnDashboard) {
         if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
+        return false;
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+        // Redirige usando URL absoluta basada en `request.url`
+        return Response.redirect(new URL('/dashboard', request.url));
       }
+    
       return true;
-    },
+    }
   },
   providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
