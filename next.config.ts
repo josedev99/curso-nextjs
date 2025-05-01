fd3d22e80de187d226a3e758d9525174369b497e
jose dev <josededanes99@gmail.com>
2025-05-01 15:59:18 -0600

    Update next.config.ts se reemplazo contenido

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,9 @@ import type { NextConfig } from 'next';
 import path from 'path';
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack(config) {
     // Agregar resolución de alias para '@'
     config.resolve.alias['@'] = path.join(__dirname, '/');
